9ec658c4c73defacfcfeaeb99a35190034e7c754
tkhe19 <tahsin.kheya@brilliant.com.bd>
2023-08-02 10:59:43 +0600

    added dockerfile, dockercompose

--- project/users/__init__.py
+++ project/users/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 users_blueprint = Blueprint("users", __name__, url_prefix="/users", template_folder="templates")
 
-from . import models  # noqa
+from . import models,tasks  # noqa
